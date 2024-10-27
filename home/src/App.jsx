8961c3cbaf8866603623399a4e2806094d6b8f22
ademsuslu <ademsuslu9080@gmail.com>
2024-10-27 14:18:27 +0300

    first commit

---
+++ home/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.scss'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="space-y-3 h-screen">Home page content</div>
+    <Footer />
+  </div>
+)
+const rootElement = document.getElementById('app')
+if (!rootElement) throw new Error('Failed to find the root element')
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<App />)
---
+++ home/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="p-3 bg-gray-500 text-white font-bold">
+      Only the bets Spinner
+    </div>
+  )
+}
+
+export default Footer
