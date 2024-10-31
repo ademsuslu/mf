c860952213a7a4f36c8fc50d406c99e04f858de9
ademsuslu <ademsuslu9080@gmail.com>
2024-10-31 21:00:48 +0300

    refactor:login

--- cart/src/components/CartContent.jsx
+++ cart/src/components/CartContent.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Login from './login'
 
 export default function CartContent() {
   const [token, setToken] = useState('')
   return (
     <div className="max-w-[200px]">
-      JWT: <p>{token}</p>
+      <Login />
     </div>
   )
 }
